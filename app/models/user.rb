e4907ded6dfee8b3e4c3af0923da47f2dab76151
Matthew <matthew.wu328@gmail.com>
2013-10-08 17:05:54 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,11 +28,4 @@ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
   user
 end
 
-def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
 end
